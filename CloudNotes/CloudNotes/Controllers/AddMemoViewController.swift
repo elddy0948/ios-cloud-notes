030a8a450e0a1b34a7150297ae12acfd8e2d0f21
Hojoon Kim <elddy0948@hotmail.co.kr>
2021-02-26 21:20:58 +0900

    feat: titleTextField, view background color 수정

--- CloudNotes/CloudNotes/Controllers/AddMemoViewController.swift
+++ CloudNotes/CloudNotes/Controllers/AddMemoViewController.swift
@@ -4,21 +4,21 @@ class AddMemoViewController: UIViewController {
     //MARK: - Views
     private let titleTextField: UITextField = {
         let textField = UITextField()
-        textField.backgroundColor = .red
+        textField.backgroundColor = .secondarySystemBackground
+        textField.font = UIFont.systemFont(ofSize: 30)
         textField.translatesAutoresizingMaskIntoConstraints = false
         return textField
     }()
     private let bodyTextView: UITextView = {
         let textView = UITextView()
-        textView.backgroundColor = .yellow
         textView.translatesAutoresizingMaskIntoConstraints = false
         return textView
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(didTapDoneButton))
-        print(navigationController?.navigationBar.isHidden)
         view.addSubview(titleTextField)
         view.addSubview(bodyTextView)
         configureConstraints()
