3ccc4cf96989a3299dd2be468e90b3bd7a4afcc9
Hojoon Kim <elddy0948@hotmail.co.kr>
2021-03-01 11:42:57 +0900

    feat: CoreDataStack.swift 파일 구현

---
+++ CloudNotes/CloudNotes/Models/CoreDataStack.swift
@@ -0,0 +1,41 @@
+import CoreData
+import Foundation
+
+class CoreDataStack {
+    private let modelName: String
+    
+    init(modelName: String) {
+        self.modelName = modelName
+    }
+    
+    lazy var managedContext: NSManagedObjectContext = {
+        return storeContainer.viewContext
+    }()
+    
+    private lazy var storeContainer: NSPersistentContainer = {
+        let container = NSPersistentCloudKitContainer(name: self.modelName)
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                print("\(error), \(error.userInfo)")
+            }
+        }
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        do {
+            try container.viewContext.setQueryGenerationFrom(.current)
+        } catch {
+            fatalError("\(error)")
+        }
+        return container
+    }()
+    
+    func saveContext() {
+        guard managedContext.hasChanges else {
+            return
+        }
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("\(error), \(error.userInfo)")
+        }
+    }
+}
